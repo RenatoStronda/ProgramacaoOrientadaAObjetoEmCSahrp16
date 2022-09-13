9662bf449552ec1ed3330fcdba13d5a9e62c1ed0
RenatoStronda <104527831+RenatoStronda@users.noreply.github.com>
2022-09-12 22:42:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace metodos{
+    class program{
+        public static void Main(String[] args){
+            Console.WriteLine("Hello, World!");
+            carro carro = new carro();
+        }
+    }
+    public class carro{
+        public float preco;
+        public string nome;
+
+        public bool ligar(){
+            console.WriteLine("Carro Ligado!");    
+        }
+    }
+}
